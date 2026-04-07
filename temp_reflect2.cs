bb146d45a4feccae1356e8cf26042bfbc9224256
R1vr4 <3331268451@qq.com>
2026-04-07 20:48:08 +0800

    chore: 删除临时反射文件

--- temp_reflect2.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Linq;
-using System.Reflection;
-var asm = Assembly.LoadFrom(@"E:\SteamLibrary\steamapps\common\Stardew Valley\Stardew Valley.dll");
-var type = asm.GetType("StardewValley.GameLocation");
-foreach (var p in type.GetProperties(BindingFlags.Public|BindingFlags.Instance).Where(p => p.Name.ToLower().Contains("furniture")).OrderBy(p => p.Name))
-    Console.WriteLine("P " + p.PropertyType + " " + p.Name);
-foreach (var f in type.GetFields(BindingFlags.Public|BindingFlags.Instance).Where(f => f.Name.ToLower().Contains("furniture")).OrderBy(f => f.Name))
-    Console.WriteLine("F " + f.FieldType + " " + f.Name);
